f189b29fcf07521450874d4d301a12bdad5516e3
Kunal <itsdexu5@gmail.com>
2020-08-25 18:19:11 +0530

    Added the recursion variant.

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     // 344. Reverse String
+    
+    // Using loop O(1)
     void reverseString(vector<char>& s) {
         for(int i=0 ; i<s.size()/2 ; i++)
         {
@@ -9,4 +11,22 @@ public:
             s[s.size() - i - 1] = temp;
         }
     }
+    
+    // Using recursion O(n)
+    
+    void helper(vector<char>& s, int left, int right){
+        if(left < right)
+        {
+            char tmp = s[left];
+            s[left++] = s[right];
+            s[right--] = tmp;
+            
+            helper(s, left, right);
+        }
+    }
+    
+    void reverseString(vector<char>& s){
+        helper(s, 0, s.size() - 1);
+    }
+    
 };
